b6d79e8e1be52ab1ea90b97cb0fbeaa45756855e
Joshua Galit <joshuaimalay@gmail.com>
2021-01-08 20:14:24 +0800

    import: Header vue componen nested Nuxt inside main tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="font-sans bg-gray-100 antialiased">
+    <Header />
+    <main>
+      <Nuxt />
+    </main>
   </div>
 </template>
 
 <script>
   export default {
+    components: {
+      Header: () => import('@/components/Header')
+    }
   }
 </script>
