3e354a70e9e7c936a18aa6d265b57df1386f5e30
Joshua Galit <joshuaimalay@gmail.com>
2021-01-08 20:09:10 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This is vercel</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: 'Vercel'
+      }
+    }
+  }
+</script>
