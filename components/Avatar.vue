85c5dab71d020a04b2f02a51a99a3c54b15cfb2c
Joshua Galit <joshuaimalay@gmail.com>
2021-01-09 08:52:09 +0800

    add: image with passig prop src, alt and some custom className for additional style option

---
+++ components/Avatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <img  class="w-8 h-8 rounded-full border border-gray-200 object-cover" 
+        :src="image" 
+        :alt="alt" 
+        :class="`${className}`"/>
+</template>
+
+<script>
+  export default {
+    props: {
+      src: {
+        type: String,
+        required: true
+      },
+      alt: {
+        type: String,
+        required: true
+      },
+      className: {
+        type: String,
+        required: false
+      }
+    },
+    computed: {
+      image () {
+        return this.src
+      }
+    }
+  }
+</script>
